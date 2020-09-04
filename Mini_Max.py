a12d5d2eec5c8d61f3571012f8327e6f706de914
rish-codes <67182874+rish-codes@users.noreply.github.com>
2020-09-04 11:53:25 +0530

    Create Mini_Max.py

---
+++ Mini_Max.py
@@ -0,0 +1,21 @@
+#
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    arr.sort()
+    print(sum(i for i in arr[0:4]), end =' ')
+    print(sum(i for i in arr[1:5]))
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
